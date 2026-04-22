22d3136f156343517280fed46dcdd0011b9e3181
Deepika Hemant Tendulkar <162742012+deepikatendulkar1@users.noreply.github.com>
2026-04-22 00:26:37 -0400

    Add: guess-number-higher-or-lower - submission-2

---
+++ Data Structures & Algorithms/guess-number-higher-or-lower/submission-2.py
@@ -0,0 +1,19 @@
+
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        l,r=1,n
+        while l<=r:
+            mid=(l+r)//2
+            res=guess(mid)
+            if res>0:
+                l=mid+1
+            elif res<0:
+                r=mid-1
+            else:
+                return mid
+                
+                
+
+            
+    
